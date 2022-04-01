f306961a2b524a08a650593f637d54c82848381c
walaniam <mariusz.walania@gmail.com>
2022-04-01 15:22:02 +0200

    Temperature sensor

--- src/weatherStation/weatherStation.ino
+++ src/weatherStation/weatherStation.ino
@@ -1,12 +1,25 @@
 #include "temperature-sensor.h"
+#include <Wire.h>
+#include "rgb_lcd.h"
+
+rgb_lcd lcd;
 
 void setup() {
+  lcd.begin(16, 2);
   Serial.begin(9600);
 }
 
 void loop() {
-  Serial.print("Current temperature: ");
+
   float currentTemp = temperature();
-  Serial.println(currentTemp);
+  
+  String message = "Celsius: ";
+  message.concat(String(currentTemp, 2));
+  
+  Serial.println(message);
+
+  lcd.clear();
+  lcd.print(message);
+  
   delay(2000);
 }
