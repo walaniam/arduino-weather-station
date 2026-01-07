2d88b914e04a98cab1b160b7e82f8963c09b1478
Mariusz Walania <mariusz.walania@gmail.com>
2026-01-07 17:52:04 +0100

    Adjust analog (outside) temperature settings

--- src/weatherStation/AnalogTemperatureSensor.cpp
+++ src/weatherStation/AnalogTemperatureSensor.cpp
@@ -1,6 +1,8 @@
 #include "AnalogTemperatureSensor.h"
 
 #define THERMISTOR_VALUE 3975
+// Adjust temperature if needed
+#define TEMP_ADJUST -2.5
 
 AnalogTemperatureSensor::AnalogTemperatureSensor(int pin) {
   sensorPin = pin;
@@ -10,5 +12,6 @@ float AnalogTemperatureSensor::measure() {
   int rawMeasure = analogRead(sensorPin);
   float resistance = (float)(1023 - rawMeasure) * 10000 / rawMeasure;
   float temp = 1 / (log(resistance / 10000) / THERMISTOR_VALUE + 1 / 298.15) - 273.15;
-  return temp;
+  float tempAdjusted = temp + 1 * TEMP_ADJUST;
+  return tempAdjusted;
 }
